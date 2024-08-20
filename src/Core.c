28b31657ad9d619477344044378012a2a1b22476
WasifBoomz <72422316+wAABBsif@users.noreply.github.com>
2024-08-20 17:38:10 -0400

    Implement Start function to Game State

--- src/Core.c
+++ src/Core.c
@@ -34,3 +34,9 @@ float InverseLerp(const float min, const float max, const float value)
 {
     return (value - min) / (max - min);
 }
+
+void SwitchState(GameData* game, const char newState)
+{
+    game->gameState = newState;
+    game->gameStates[game->gameState].Start(game);
+}
--- src/SaveData.c
+++ src/SaveData.c
@@ -1,5 +1,4 @@
 #include "SaveData.h"
-
 #include <stdio.h>
 
 void SavePlayerData(SaveData saveData)
@@ -10,10 +9,11 @@ void SavePlayerData(SaveData saveData)
 void LoadPlayerData(SaveData* saveData)
 {
     FILE* stream = fopen(PLAYER_DATA_FILENAME, "r");
-    if (!stream)
+    if (stream == NULL)
     {
         printf("Could not load player data!\n");
         return;
     }
+
     fread(saveData, sizeof(SaveData), sizeof(SaveData), stream);
 }
